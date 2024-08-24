72a92218f1f6b2e78d2aa03b9a8ffb5024e84516
Silas-Silence <silas-boecher@t-online.de>
2024-08-24 16:29:14 +0200

    Added sprinting and slight camera adjustments

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] TMP_Text debugText;
+
     [Header("Movement")]
     public float moveSpeed = 12;
     public float sprintSpeedMultiplier = 2;
     public float groundDrag;
 
+    [SerializeField] KeyCode _sprintKey = KeyCode.LeftShift;
+    [SerializeField] bool _isSprinting;
+
     [Header("Jump")]
     public LayerMask groundLayer;
     public float jumpForce = 25;
     public float airDrag;
-    
+
     [SerializeField] KeyCode _jumpKey = KeyCode.Space;
     [SerializeField] float _lowJumpMultiplier;
     [SerializeField] float _fallMultiplier;
@@ -31,52 +37,65 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection = transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
 
+        _isSprinting = Input.GetKey(_sprintKey);
         _grounded = Physics.Raycast(_groundCheckPosition.position, Vector3.down, .2f, groundLayer);
         rb.drag = _grounded ? groundDrag : airDrag;
 
-        if(Input.GetKey(_jumpKey) && _grounded && _canJump){Jump();}
-        if(!_grounded){ImproveJump();}
+        if (Input.GetKey(_jumpKey) && _grounded && _canJump) { Jump(); }
+        if (!_grounded) { ImproveJump(); }
 
+        debugText.text = $"Speed: {rb.velocity.magnitude}";
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         MovePlayer();
         SpeedControl();
-        
+
     }
 
-    void MovePlayer(){
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    void MovePlayer()
+    {
+        rb.AddForce(moveDirection.normalized * moveSpeed * (_isSprinting ? sprintSpeedMultiplier : 1) * 10f, ForceMode.Force);
     }
 
-    void SpeedControl(){
+    void SpeedControl()
+    {
+        float multiplier = moveSpeed * (_isSprinting ? sprintSpeedMultiplier : 1);
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        if(flatVel.magnitude > moveSpeed){
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * multiplier;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
 
-    void Jump(){
+    void Jump()
+    {
         _canJump = false;
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); 
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
 
         Invoke("ResetJump", _jumpCooldown);
     }
 
-    void ImproveJump(){
+    void ImproveJump()
+    {
         float multiplier = 1;
-        if(rb.velocity.y < 0){
+        if (rb.velocity.y < 0)
+        {
             multiplier = _fallMultiplier;
-        }else if (rb.velocity.y > 0 && !Input.GetKey(_jumpKey)){
-            multiplier = _lowJumpMultiplier; 
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(_jumpKey))
+        {
+            multiplier = _lowJumpMultiplier;
         }
 
         rb.velocity += Vector3.up * Physics.gravity.y * (multiplier - 1) * Time.deltaTime;
     }
 
-    void ResetJump(){
+    void ResetJump()
+    {
         _canJump = true;
     }
 }
