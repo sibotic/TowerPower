29777a29e7b98da78b602d15ece84229f2192e40
Silas-Silence <silas-boecher@t-online.de>
2024-08-24 14:50:05 +0200

    Added a jump that feels way better

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,33 +2,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [Header("Movement")]
     public float moveSpeed = 12;
     public float sprintSpeedMultiplier = 2;
+    public float groundDrag;
 
     [Header("Jump")]
-    public float jumpForce = 15;
+    public LayerMask groundLayer;
+    public float jumpForce = 25;
+    public float airDrag;
+    
+    [SerializeField] KeyCode _jumpKey = KeyCode.Space;
+    [SerializeField] float _lowJumpMultiplier;
+    [SerializeField] float _fallMultiplier;
+    [SerializeField] Transform _groundCheckPosition;
+    bool _grounded, _canJump = true;
+    float _jumpCooldown = .25f;
 
     Rigidbody rb;
     Vector3 moveDirection;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         moveDirection = transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
-        if(Input.GetKeyDown(KeyCode.Space)){Jump();}
+
+        _grounded = Physics.Raycast(_groundCheckPosition.position, Vector3.down, .2f, groundLayer);
+        rb.drag = _grounded ? groundDrag : airDrag;
+
+        if(Input.GetKey(_jumpKey) && _grounded && _canJump){Jump();}
+        if(!_grounded){ImproveJump();}
+
     }
 
     void FixedUpdate(){
         MovePlayer();
         SpeedControl();
+        
     }
 
     void MovePlayer(){
@@ -44,6 +58,25 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Jump(){
+        _canJump = false;
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+        Invoke("ResetJump", _jumpCooldown);
+    }
+
+    void ImproveJump(){
+        float multiplier = 1;
+        if(rb.velocity.y < 0){
+            multiplier = _fallMultiplier;
+        }else if (rb.velocity.y > 0 && !Input.GetKey(_jumpKey)){
+            multiplier = _lowJumpMultiplier; 
+        }
+
+        rb.velocity += Vector3.up * Physics.gravity.y * (multiplier - 1) * Time.deltaTime;
+    }
+
+    void ResetJump(){
+        _canJump = true;
     }
 }
